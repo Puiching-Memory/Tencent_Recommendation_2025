62057f1c12bf084ef803fbf382b9274066bdc736
梦归云帆 <1138663075@qq.com>
2025-08-19 23:55:35 +0800

    feat(eval): 添加评估数据集和推理代码
    - 新增 MyDataset 和 MyTestDataset 类用于处理评估数据
    - 添加 infer.py 文件实现推理功能
    - 更新 README.md 中的模型精度信息

--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ echo ${RUNTIME_SCRIPT_DIR}
 # enter train workspace
 cd ${RUNTIME_SCRIPT_DIR}
 
-# write your code below
-python -u main.py
+python -u main.py --hidden_units 64 --num_blocks 2 --num_heads 4 --batch_size 256 --lr 0.0005 --num_epochs 10 --dropout_rate 0.1 --l2_emb 0.001 --mm_emb_id 81 82 83 84
